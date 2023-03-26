f68526976be571421ea7ca56dd372913e356c939
Arda Kestane <85238789+ArdaKestane@users.noreply.github.com>
2023-03-26 19:40:35 +0300

    resultPage and navigation buttons

--- src/main/java/com/example/dictionary/Controller.java
+++ src/main/java/com/example/dictionary/Controller.java
@@ -2,13 +2,28 @@ package com.example.dictionary;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
+    private VBox landingPage;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private VBox resultPage;
+
+    public void initialize() {
+        
     }
+
+    public void home(){
+        landingPage.setVisible(true);
+        resultPage.setVisible(false);
+    }
+
+    public void search(){
+        landingPage.setVisible(false);
+        resultPage.setVisible(true);
+    }
+
+
 }
